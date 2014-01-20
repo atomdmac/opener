d95cd5ca62a5757280213bee081051d89203636f
suicidepills <atom.mac@gmail.com>
2014-01-20 12:39:25 -0500

    Add opener.cpp.
    Signed-off-by: suicidepills <atom.mac@gmail.com>

---
+++ opener.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+#include <windows.h>
+#include <stdio.h>
+#include <tchar.h>
+
+using namespace std;
+
+// int _tmain( int argc, TCHAR *argv[] )
+int main ( int argc, char argv[] )
+{
+    STARTUPINFO si;
+    PROCESS_INFORMATION pi;
+
+    ZeroMemory( &si, sizeof(si) );
+    si.cb = sizeof(si);
+    ZeroMemory( &pi, sizeof(pi) );
+    /*
+    if( argc != 2 )
+    {
+        printf("Usage: %s [cmdline]\n", argv[0]);
+        return 0;
+    }
+    */
+    
+    string argz = "nw.exe package.json " + *argv;
+    cout << "why doesn't this work?" << argv[1];
+    
+    // Start the child process. 
+    if( !CreateProcess( NULL,   // No module name (use command line)
+        argv[1],        // Command line
+        NULL,           // Process handle not inheritable
+        NULL,           // Thread handle not inheritable
+        FALSE,          // Set handle inheritance to FALSE
+        0,              // No creation flags
+        NULL,           // Use parent's environment block
+        NULL,           // Use parent's starting directory 
+        &si,            // Pointer to STARTUPINFO structure
+        &pi )           // Pointer to PROCESS_INFORMATION structure
+    ) 
+    {
+        printf( "CreateProcess failed (%d).\n", GetLastError() );
+        return 1;
+    }
+
+    // Wait until child process exits.
+    WaitForSingleObject( pi.hProcess, INFINITE );
+
+    // Close process and thread handles. 
+    CloseHandle( pi.hProcess );
+    CloseHandle( pi.hThread );
+}
